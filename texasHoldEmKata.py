bbaa5e78e9387abfd1b9cdb216e6be51447cc2e8
ericq1 <erickmartin.2@gmail.com>
2014-10-31 00:52:10 -0700

    Update texasHoldEmKata.py

--- texasHoldEmKata.py
+++ texasHoldEmKata.py
@@ -4,7 +4,7 @@ def GetRankedGame(hands):
         que juego tiene cada mano, darles puntajes y determinar cual gana
         (regresa una cadena con el formato indicado) 
     """
-    raise NotImplementedError
+    
     
 if __name__ == '__main__':
     print("Captura un mano por linea y una linea en blanco para terminar:")
